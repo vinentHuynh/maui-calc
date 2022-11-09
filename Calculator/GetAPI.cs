defce18ecf0f0f2b08b7c2893f0b8bdcc680b0b2
lekevin <lekevvin@gmail.com>
2022-11-08 19:09:33 -0600

    set base url for requests, deserialize resource

---
+++ Calculator/GetAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Linq;
+
+namespace Calculator
+{
+    public class GetAPI
+    {
+        public async Task<ExercisesData> GetData(int id)
+        {
+            ExercisesData data = new ExercisesData();
+             var client = new HttpClient();
+
+           
+                string url = "http://localhost:5009/" + id;
+                
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                
+                
+                var content = await response.Content.ReadAsStringAsync();
+                
+                data = JsonSerializer.Deserialize<ExercisesData>(content);
+                return data;
+            
+            
+        }
+    }
+}
