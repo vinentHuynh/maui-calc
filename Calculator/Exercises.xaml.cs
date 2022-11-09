e5ea478556c337e1250ca9d02b7a78e897720be0
lekevin <lekevvin@gmail.com>
2022-11-08 19:26:29 -0600

    added backend logic for parsed exercise data

--- Calculator/Exercises.xaml.cs
+++ Calculator/Exercises.xaml.cs
@@ -1,9 +1,51 @@
 ﻿namespace Calculator;
 
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
 public partial class Exercises : ContentPage
 {
-	public Exercises()
-	{
-		InitializeComponent();
-	}
+    public int id = 1;
+    public Exercises()
+    {
+        InitializeComponent();
+        getExercise();
+
+    }
+    async void getExercise()
+    {
+        ExercisesData data = await callApi();
+        questionNumber.Text = "Question " + id.ToString();
+        choice1.Text = data.incorrect1.ToString();
+        choice2.Text = data.incorrect2.ToString();
+        choice3.Text = data.correct.ToString();
+        questionContent.Text = data.equation;
+
+    }
+    async Task<ExercisesData> callApi()
+    {
+        GetAPI getAPI = new GetAPI();
+        ExercisesData value = await getAPI.GetData(id);
+        return value;
+
+    }
+    void isCorrect(object sender, EventArgs e)
+    {
+        DataTable dt = new DataTable();
+        Button button = (Button)sender;
+        string pressed = button.Text;
+
+        if (dt.Compute(questionContent.Text, "").ToString() == pressed)
+        {
+            id++;
+            //animation here
+            if (id == 11)
+            {
+                id = 1;
+            }
+            getExercise();
+        }
+
+    }
 }
