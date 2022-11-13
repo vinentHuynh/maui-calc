830be7a0a8e8bc5886e71980212eefa1203e87ab
vincenthuynh <vincenthuynh02@gmail.com>
2022-11-13 01:17:19 -0600

    added models and viewmodel

---
+++ Calculator/Models/History.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Calculator.Models
+{
+	public class History
+	{
+		public string historyString { get;set; }
+
+	}
+}
+
---
+++ Calculator/ViewModel/HistoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Calculator.Models;
+namespace Calculator.ViewModel
+{
+	public class HistoryViewModel
+	{
+		private IList<Models.History> historyModel;
+		public HistoryViewModel(string equation)
+		{
+			new History{ historyString=equation};
+		}
+	}
+}
+
