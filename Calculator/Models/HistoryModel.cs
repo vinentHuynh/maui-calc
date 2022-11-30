f6090d7a6273dcd150594496235ca120020d041f
Vincent Huynh <vincenthuynh0@gmail.com>
2022-11-30 16:08:43 -0600

    Added database model and connection class

--- Calculator/Models/HistoryModel.cs
+++ Calculator/Models/HistoryModel.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.ComponentModel;
+﻿using SQLite;
 namespace Calculator.Models
 {
+	[Table("history")]
 	public class HistoryModel
 	{
-	
+		[PrimaryKey, AutoIncrement,Column("_id")]
+		public int Id { get; set; }
 		public string historyString { get;set; }
 
 	}
