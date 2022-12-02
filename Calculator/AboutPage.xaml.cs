7a472e4e244589344007cf3861f5822f61325253
lekevin <lekevvin@gmail.com>
2022-12-02 12:44:21 -0600

    added scrolling, learn more button function
    added scrolling, included buttons, trying to mimic https://www.starbucks.com/about-us/

--- Calculator/AboutPage.xaml.cs
+++ Calculator/AboutPage.xaml.cs
@@ -1,10 +1,25 @@
 ﻿namespace Calculator;
+using System;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Windows.Input;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
+	private async void OnLearnMoreClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Uri uri = new Uri("https://www.starbucks.com/about-us/");
+			await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+		}
+		catch (Exception ex)
+		{ //Error occured
+		}
+	}
+    public AboutPage()
 	{
 		InitializeComponent();
-
 	}
 }
